264ee263728d06c3a6f618497216e57f4aa4e407
snowsolf <snowsolf@hotmail.com>
2015-12-01 13:50:07 +0800

    Update

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Canvas clock
 
 这是一个利用HTML5 Canvas写的钟表。
+
+## 应用实例
+[http://www.cnblogs.com/snowsolf](http://www.cnblogs.com/snowsolf)
--- js/clock.js
+++ js/clock.js
@@ -1,44 +1,57 @@
 /**
+ * Canvas clock.
  * Created by snowsolf(snowsolf@hotmail.com) on 2015/11/28.
  */
 
-var radius = 100;
+var radius = 90;
 var pi = Math.PI;
 window.onload = function() {
 	var canvas = document.getElementById('canvas');
-	canvas.height = document.body.clientHeight;
-	canvas.width = document.body.clientWidth;
+	canvas.height = 210;
+	canvas.width = 210;
 
 	var ctx = canvas.getContext("2d");
+	initCanvas(canvas, ctx);
+	drawHourScale(ctx);
+	drawMinuteScale(ctx);
 
 	setInterval(function () {
-		initCanvas(canvas, ctx);
+		clearCanvasInside(ctx);
 		drawCenterDot(ctx);
-		drawHourScale(ctx);
-		drawMinuteScale(ctx);
 		var date = new Date();
 		drawHourHand(ctx, date);
 		drawMinuteHand(ctx, date);
 		drawSecondHand(ctx, date);
-	}, 1000);
+	}, 50);
 };
 
 function initCanvas(canvas, ctx) {
 	ctx.fillStyle = "#FFFFFF";
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
 	ctx.beginPath();
-	ctx.arc(200, 200, radius, 0, 2 * pi, true);
-	ctx.shadowOffsetX = 8;
-	ctx.shadowOffsetY = 8;
+	ctx.arc(105, 105, radius, 0, 2 * pi, true);
+	ctx.shadowOffsetX = 5;
+	ctx.shadowOffsetY = 5;
 	ctx.shadowBlur = 20;
 	ctx.shadowColor = "#494844";
+	ctx.fillStyle = "#FFFFFF";
+	ctx.fill();
+	ctx.shadowOffsetX = 0;
+	ctx.shadowOffsetY = 0;
+	ctx.shadowBlur = 0;
+	ctx.shadowColor = "#FFFFFF";
+}
+
+function clearCanvasInside(ctx) {
+	ctx.beginPath();
+	ctx.arc(105, 105, radius - 19, 0, 2 * pi, true);
 	ctx.fillStyle = "#FFFFFF";
 	ctx.fill();
 }
 
 function drawCenterDot(ctx) {
 	ctx.beginPath();
-	ctx.arc(200, 200, 4, 0, 2 * pi, true);
+	ctx.arc(105, 105, 4, 0, 2 * pi, true);
 	ctx.fillStyle = "#000000";
 	ctx.fill();
 }
@@ -58,10 +71,10 @@ function drawMinuteScale(ctx) {
 }
 
 function drawScale(ctx, radius, angle, width, length) {
-	var startX = (radius - length) * Math.sin(angle) + 200;
-	var startY = (radius - length) * Math.cos(angle) + 200;
-	var endX = radius * Math.sin(angle) + 200;
-	var endY = radius * Math.cos(angle) + 200;
+	var startX = (radius - length) * Math.sin(angle) + 105;
+	var startY = (radius - length) * Math.cos(angle) + 105;
+	var endX = radius * Math.sin(angle) + 105;
+	var endY = radius * Math.cos(angle) + 105;
 
 	ctx.beginPath();
 	ctx.moveTo(startX, startY);
@@ -76,13 +89,12 @@ function drawHourHand(ctx, date) {
 	var angle = (-2) * pi *
 		(date.getSeconds() + date.getMinutes() * 60 +
 			(date.getHours() > 11 ? (date.getHours() - 12) : date.getHours()) * 3600
-		) /
-		(3600 * 12);
-	var endX = (radius - 50) * Math.sin(angle + pi) + 200;
-	var endY = (radius - 50) * Math.cos(angle + pi) + 200;
+		) /	(3600 * 12);
+	var endX = (radius - 40) * Math.sin(angle + pi) + 105;
+	var endY = (radius - 40) * Math.cos(angle + pi) + 105;
 
 	ctx.beginPath();
-	ctx.moveTo(200, 200);
+	ctx.moveTo(105, 105);
 	ctx.lineTo(endX, endY);
 	ctx.lineWidth = 6;
 	ctx.strokeStyle = "#000000";
@@ -91,11 +103,11 @@ function drawHourHand(ctx, date) {
 
 function drawMinuteHand(ctx, date) {
 	var angle = (-2) * pi * (date.getSeconds() + date.getMinutes() * 60) / 3600;
-	var endX = (radius - 30) * Math.sin(angle + pi) + 200;
-	var endY = (radius - 30) * Math.cos(angle + pi) + 200;
+	var endX = (radius - 30) * Math.sin(angle + pi) + 105;
+	var endY = (radius - 30) * Math.cos(angle + pi) + 105;
 
 	ctx.beginPath();
-	ctx.moveTo(200, 200);
+	ctx.moveTo(105, 105);
 	ctx.lineTo(endX, endY);
 	ctx.lineWidth = 4;
 	ctx.strokeStyle = "#000000";
@@ -103,12 +115,12 @@ function drawMinuteHand(ctx, date) {
 }
 
 function drawSecondHand(ctx, date) {
-	var angle = (-1) * pi * date.getSeconds() / 30;
-	var endX = (radius - 20) * Math.sin(angle + pi) + 200;
-	var endY = (radius - 20) * Math.cos(angle + pi) + 200;
+	var angle = (-2) * pi * (date.getSeconds() * 1000 + date.getMilliseconds()) / (60 * 1000);
+	var endX = (radius - 20) * Math.sin(angle + pi) + 105;
+	var endY = (radius - 20) * Math.cos(angle + pi) + 105;
 
 	ctx.beginPath();
-	ctx.moveTo(200, 200);
+	ctx.moveTo(105, 105);
 	ctx.lineTo(endX, endY);
 	ctx.lineWidth = 2;
 	ctx.strokeStyle = "#000000";
